921c3cb40ee0b8ea5bd2befcd0d4c0c433fa1c28
breonw <breon_williams@unc.edu>
2016-04-27 12:18:54 -0400

    modify thumbnails

--- assets/functions.php
+++ assets/functions.php
@@ -20,7 +20,7 @@ add_filter('the_content', 'clean_shortcodes');
 
 if ( function_exists( 'add_theme_support' ) ) {
     add_image_size( 'post_thumbnail', 64, 64, true ); // Soft Crop Mode
-    add_image_size( 'post_thumbnail_lg', 400, 300, true ); // Soft Crop Mode
+    add_image_size( 'post_thumbnail_lg', 290, 200, true ); // Soft Crop Mode
 }
 
 /* Full Width Color Section */
